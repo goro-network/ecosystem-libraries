1193a4079c49fa720e7df3f8cf0dbe1dbd85f9fe
Aditya Kresna <aditya.kresna@outlook.co.id>
2023-11-12 15:19:26 +0700

    feat(crate-logging): use gloo-console for wasm32

--- crate-logging/README.md
+++ crate-logging/README.md
@@ -6,5 +6,5 @@ Convenience library for logging
 
 Either:
 
-- `default` which enables [env_logger](https://crates.io/crates/env_logger)
-- `wasm32` which enables [console_log](https://crates.io/crates/console_log)
+- `main` which enables [env_logger](https://crates.io/crates/env_logger)
+- `wasm32` which enables [gloo-console](https://crates.io/crates/gloo-console)
--- crate-logging/src/lib.rs
+++ crate-logging/src/lib.rs
@@ -1,13 +1,19 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 #![forbid(unsafe_code)]
 #![deny(warnings)]
 
-#[cfg(all(feature = "default", feature = "wasm32"))]
-compile_error!("Feature \"default\" can't be combined with \"wasm32\".");
+#[cfg(all(feature = "main", feature = "wasm32"))]
+compile_error!("Feature \"main\" can't be combined with \"wasm32\".");
 
+#[cfg(feature = "wasm32")]
+pub use gloo_console::{debug, error, info, warn};
+#[cfg(not(feature = "wasm32"))]
 pub use log::{debug, error, info, warn};
 
+#[cfg(feature = "main")]
 const ENVKEY_RUST_LOG: &str = "RUST_LOG";
 
+#[cfg(feature = "main")]
 pub fn init() {
     if std::env::var(ENVKEY_RUST_LOG).is_err() {
         #[cfg(debug_assertions)]
@@ -16,13 +22,10 @@ pub fn init() {
         std::env::set_var(ENVKEY_RUST_LOG, "info");
     }
 
-    #[cfg(feature = "default")]
     env_logger::builder()
         .default_format()
         .format_timestamp_micros()
         .format_indent(Some(2))
         .parse_env(ENVKEY_RUST_LOG)
         .init();
-    #[cfg(feature = "wasm32")]
-    console_log::init().unwrap();
 }
